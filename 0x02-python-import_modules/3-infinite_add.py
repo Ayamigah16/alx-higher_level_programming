e9f64cf33ee06722b77e280f9c2c027c1c72e83f
Abraham Ayamigah <abkc070@gmail.com>
2023-06-10 07:33:13 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,8 @@ if __name__ == "__main__":
     """ Add all command line arguments """
     # initializing sum as zero
     sum = 0
-
     # get sum
     for i in range(len(sys.argv) - 1):
-        sum += int(sys.argv(i + 1))
+        sum += int(sys.argv[i + 1])
     # print sum
     print("{}".format(sum))
