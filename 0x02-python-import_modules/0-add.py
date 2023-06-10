726e5cfd4fe1608d1319367fec21c699a650f1ff
Abraham Ayamigah <abkc070@gmail.com>
2023-06-10 05:40:56 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    # code will run only when 0-add.py is run
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    
