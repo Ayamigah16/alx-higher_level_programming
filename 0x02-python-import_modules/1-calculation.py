c335b8bea243f0adfa54423d3d51138f0b353945
Abraham Ayamigah <abkc070@gmail.com>
2023-06-10 07:13:03 -0700

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+
+if __name__ == "__main__":
+    # code will execute when 1-calculation is run
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
