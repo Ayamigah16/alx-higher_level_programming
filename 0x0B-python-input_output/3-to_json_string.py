662d5002a90d5adf01a816a2b32633c2a0081b2d
Abraham Ayamigah <abkc070@gmail.com>
2023-07-11 20:01:30 -0700

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -15,3 +15,31 @@ def to_json_string(my_obj):
         str: The JSON representation of the object.
     """
     return json.dumps(my_obj)
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3]
+    s_my_list = to_json_string(my_list)
+    print(s_my_list)
+    print(type(s_my_list))
+
+    my_dict = {
+        'id': 12,
+        'name': "John",
+        'places': ["San Francisco", "Tokyo"],
+        'is_active': True,
+        'info': {
+            'age': 36,
+            'average': 3.14
+        }
+    }
+    s_my_dict = to_json_string(my_dict)
+    print(s_my_dict)
+    print(type(s_my_dict))
+
+    try:
+        my_set = {132, 3}
+        s_my_set = to_json_string(my_set)
+        print(s_my_set)
+        print(type(s_my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
