a53d8e641054cc2776b40d14d69c92bcf26b19ea
Abraham Ayamigah <abkc070@gmail.com>
2023-07-29 07:13:15 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,38 @@
-#!/usr/bin/python3
-"""Program to save strings from command line arguments to file called
-`add_item.json`. File contains a json serialized list of all strings
-entered as arguments to the program.
+#!/usr/bin/env python3
+
+"""Script to add items to a JSON file.
+
+This script takes command-line arguments and adds them to a list.
+The list is then saved to a JSON file named "add_item.json".
+If the file already exists, the script loads its contents into the list before adding the new items.
+If the file does not exist, it initializes an empty list and then appends the command-line arguments.
+
+Usage:
+    ./add_item.py item1 item2 item3 ...
+
+Author: [Your Name]
 """
+
 import sys
-from os import path
+import os.path
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
-
-if __name__ == '__main__':
-    """
-    Script to add command-line arguments to a Python
-    list and save them to a file.
-    """
-
+def main():
+    # The filename to use for the JSON file
     filename = 'add_item.json'
 
-    if path.exists(filename):
+    # Check if the JSON file exists and load its contents
+    if os.path.exists(filename):
         my_list = load_from_json_file(filename)
     else:
         my_list = []
 
+    # Extend the list with the command-line arguments provided
     my_list.extend(sys.argv[1:])
+
+    # Save the updated list to the JSON file
     save_to_json_file(my_list, filename)
+
+if __name__ == '__main__':
+    main()
