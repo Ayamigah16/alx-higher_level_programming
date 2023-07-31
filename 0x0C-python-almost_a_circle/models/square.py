8f42880453ae42cc6b792818df3f8745c02d68f9
Abraham Ayamigah <abkc070@gmail.com>
2023-07-31 12:00:02 -0700

    update on square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,16 +8,23 @@ class Square(Rectangle):
     """Square class that inherits from Rectangle.
 
     Attributes:
-        size (int): Private instance attribute representing the size of the square.
-        x (int): Private instance attribute representing the x-coordinate of the square.
-        y (int): Private instance attribute representing the y-coordinate of the square.
-        id (int): Public instance attribute representing the object's ID.
+        size (int): Private instance attribute
+    representing the size of the square.
+        x (int): Private instance attribute
+    representing the x-coordinate of the square.
+        y (int): Private instance attribute
+    representing the y-coordinate of the square.
+        id (int): Public instance attribute
+    representing the object's ID.
 
     Methods:
-        __init__(self, size, x=0, y=0, id=None): Initializes a new instance of the Square class.
-        __str__(self): Returns a string representation of the square.
+        __init__(self, size, x=0, y=0, id=None):
+    Initializes a new instance of the Square class.
+        __str__(self): Returns a string representation
+    of the square.
         size(self): Getter method for the size attribute.
-        size(self, value): Setter method for the size attribute.
+        size(self, value): Setter method for the size
+    attribute.
 
     """
 
@@ -26,9 +33,12 @@ class Square(Rectangle):
 
         Args:
             size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square. Defaults to 0.
-            y (int, optional): The y-coordinate of the square. Defaults to 0.
-            id (int, optional): The object's ID. Defaults to None.
+            x (int, optional): The x-coordinate of
+        the square. Defaults to 0.
+            y (int, optional): The y-coordinate of
+        the square. Defaults to 0.
+            id (int, optional): The object's ID.
+        Defaults to None.
 
         """
         super().__init__(size, size, x, y, id)
@@ -60,4 +70,5 @@ class Square(Rectangle):
             str: The string representation of the square.
 
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
