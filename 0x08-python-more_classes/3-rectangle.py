3a0d0c4ecae4c10aa202aafa71e04484f7ba2ff9
Abraham Ayamigah <abkc070@gmail.com>
2023-10-03 10:18:15 -0700

    rsolving bug

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -96,7 +96,7 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(["#" * self.__width] * self.__height)
+        return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """
