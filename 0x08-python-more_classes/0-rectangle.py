f2af46cc0fb7c5c30573313880c96214c3f671d0
Abraham Ayamigah <abkc070@gmail.com>
2023-10-03 09:59:18 -0700

    0. Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Rectangle Module
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty Rectangle object
+        """
+        pass
