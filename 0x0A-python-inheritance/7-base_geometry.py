7d2c964c10c55bd8aee88c41f82e841ec52e5cbc
Abraham Ayamigah <abkc070@gmail.com>
2023-10-11 17:43:06 -0700

    bug fixed

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -27,3 +27,4 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        return value
