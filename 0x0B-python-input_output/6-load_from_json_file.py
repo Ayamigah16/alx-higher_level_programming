21b4e870c71e644ba47f3f26bc739f54fac4b48c
Abraham Ayamigah <abkc070@gmail.com>
2023-10-11 18:51:40 -0700

    6. Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Contains the "load_from_json_file" function
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The object created from the JSON file.
+    """
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)
+
+
+if __name__ == '__main__':
+    filename = "my_list.json"
+    my_list = load_from_json_file(filename)
+    print(my_list)
+    print(type(my_list))
+
+    filename = "my_dict.json"
+    my_dict = load_from_json_file(filename)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        filename = "my_set_doesnt_exist.json"
+        my_set = load_from_json_file(filename)
+        print(my_set)
+        print(type(my_set))
+    except FileNotFoundError as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        filename = "my_fake.json"
+        my_fake = load_from_json_file(filename)
+        print(my_fake)
+        print(type(my_fake))
+    except ValueError as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
