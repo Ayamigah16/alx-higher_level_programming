db8544fb1e05cb6f4159851edfb38881addea405
Abraham Ayamigah <abkc070@gmail.com>
2023-11-14 17:56:48 -0800

    18. Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- 101-avg_temperatures.sql
+
+-- Displaying the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
