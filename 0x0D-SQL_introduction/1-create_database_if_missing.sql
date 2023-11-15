2ec61b6be44cda88c3f517338fbdf5720f15e712
Abraham Ayamigah <abkc070@gmail.com>
2023-11-14 17:23:55 -0800

    1. Create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+-- 1-create_database_if_missing.sql
+
+-- Creating the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Switching to the newly created database
+USE hbtn_0c_0;
