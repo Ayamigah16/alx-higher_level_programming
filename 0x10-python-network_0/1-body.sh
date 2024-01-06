51956ef4107ca13b49b338e5af3ad95bf20e7b43
Abraham Ayamigah <abrahamayamigah070@gmail.com>
2024-01-05 16:06:46 -0800

    task 6

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response (only for a 200 status code)
-curl -sL -w "%{http_code}" "$1" -o /dev/null
+curl -sL "$1"
