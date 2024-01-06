c0385c97be49eb1aed97c8df636e10b07af7f952
Abraham Ayamigah <abrahamayamigah070@gmail.com>
2024-01-05 16:27:26 -0800

    made a 3-line scripts

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL and displays only the status code of the response
-
-url="$1"
-
-# Send the request and extract the status code
-curl -s -o /dev/null -w "%{http_code}" "$url"
+curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL with the contents of a file and displays the body of the response
-
-url="$1"
-file="$2"
-
-# Check if the file is a valid JSON
-if jq empty < "$file" >/dev/null 2>&1; then
-    # Send the POST request with the file contents in the body
-    curl -sX POST -H "Content-Type: application/json" -d @"$file" "$url"
-else
-    echo "Not a valid JSON"
-fi
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
