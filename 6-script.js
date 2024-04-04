29b444298531427f5351e495b5bbad4ee4874a8d
Abraham Ayamigah <abkc070@gmail.com>
2024-04-03 18:25:38 -0700

    6. Change the text

---
+++ 6-script.js
@@ -0,0 +1,8 @@
+// Wait for the document to be ready
+$(document).ready(function() {
+    // Attach a click event handler to the div with id update_header
+    $('#update_header').click(function() {
+        // Update the text of the header element
+        $('header').text('New Header!!!');
+    });
+});
