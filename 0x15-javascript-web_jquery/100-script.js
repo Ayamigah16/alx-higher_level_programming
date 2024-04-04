0add58f7077b77c56900b1041e010e88193b1b56
Abraham Ayamigah <abkc070@gmail.com>
2024-04-03 18:35:08 -0700

    10. No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the header element
+    var header = document.querySelector('header');
+    // Update the text color to red
+    header.style.color = '#FF0000';
+});
